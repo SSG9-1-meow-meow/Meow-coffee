9e5630c67f59afd2677b93c1f4dd40d3ca591156
Hyeonbin2379 <hyeonbin2379@gmail.com>
2025-11-08 17:50:41 +0900

    feat : 로그인/회원관리 기능 관련 DTO 추가

---
+++ src/main/java/com/ssg/meowcoffee/dto/UserPageDTO.java
@@ -0,0 +1,38 @@
+package com.ssg.meowcoffee.dto;
+
+import java.util.List;
+import javax.validation.constraints.Min;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class UserPageDTO<E> {
+
+    private int page;
+    private int amount;
+    private int startPage;  //시작
+    private int endPage;    //화면상 마지막 번호
+    private boolean prev, next;
+
+    private int total;
+    private List<E> dtoList;
+
+    @Builder
+    public UserPageDTO(UserCriteria cri, List<E> dtoList, int total) {
+        this.page = cri.getPageNum();
+        this.amount = cri.getAmount();
+        this.total = total;
+        this.dtoList = dtoList;
+
+        this.endPage = (int) (Math.ceil(cri.getPageNum() / 10.0)) * 10;
+        this.startPage = this.endPage - 9;
+
+        int realEnd = (int)(Math.ceil((double) total / cri.getAmount()));
+        this.endPage = Math.min(realEnd, this.endPage);
+
+        this.prev = this.startPage > 1;
+        this.next = this.endPage < realEnd;
+    }
+}
