8bda82c649b0ae122a7778b355ab2782940220b2
Hyeonbin2379 <hyeonbin2379@gmail.com>
2025-11-09 21:13:41 +0900

    refactor: 회원정보/회원상태 변경 메서드의 반환타입 변경

--- src/main/java/com/ssg/meowcoffee/service/MemberService.java
+++ src/main/java/com/ssg/meowcoffee/service/MemberService.java
@@ -18,9 +18,9 @@ public interface MemberService {
     CompanyDetailDTO getCompanyById(String userId);
     DeliverymanDTO getDeliverymenById(String userId);
 
-    void modifyUser(UserInfoUpdateDTO userInfoUpdateDTO);           // 회원정보 변경
-    void modifyUserStatus(UserStatUpdateDTO userStatUpdateDTO);     // 회원상태 변경(총관리자 전용)
+    boolean modifyUser(UserInfoUpdateDTO userInfoUpdateDTO);           // 회원정보 변경
+    boolean modifyUserStatus(UserStatUpdateDTO userStatUpdateDTO);     // 회원상태 변경(총관리자 전용)
 
-    void deactivateUser(String currentId);          // 휴면회원 전환 신청
-    void deactivateUserByAdmin(String targetId);    // 휴면회원 전환(총관리자 전용)
+    boolean deactivateUser(String currentId);          // 휴면회원 전환 신청
+    boolean deactivateUserByAdmin(String targetId);    // 휴면회원 전환(총관리자 전용)
 }
