0f3ae86ea7eed1219f498325759684f0f207ca76
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-11-13 15:39:54 +0900

    feat(auth): 배송기사 뷰 속성 추가 -> DeliverymanVO delivEmail 필드 추가 및 관련 mapper 테스트 완료

--- src/main/java/com/ssg/meowcoffee/domain/DeliverymanVO.java
+++ src/main/java/com/ssg/meowcoffee/domain/DeliverymanVO.java
@@ -17,6 +17,7 @@ public class DeliverymanVO {
     private String delivName;
     private String delivPwd;
     private String delivPhone;
+    private String delivEmail;
     private String delivCode;
     private String delivImgPath;
     private String delivVhcId;
