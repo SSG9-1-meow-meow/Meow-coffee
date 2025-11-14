13c4b2871753d0ba07d689cd9e00a63d31a1452b
Hyeonbin2379 <hyeonbin2379@gmail.com>
2025-11-15 00:37:09 +0900

    fix(auth): 로그인 사용자 권한 확인 오류 수정

---
+++ src/main/java/com/ssg/meowcoffee/service/AuthService.java
@@ -0,0 +1,27 @@
+package com.ssg.meowcoffee.service;
+
+import com.ssg.meowcoffee.dto.CustomUserDetails;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class AuthService {
+
+    public CustomUserDetails getCurrentUser() throws ClassCastException {
+        return (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public String getCurrentUserId() {
+        CustomUserDetails principal = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return principal.getUserId();
+    }
+
+    public String getCurrentUserRole() {
+        CustomUserDetails principal = (CustomUserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        return principal.getUserRole().getRoleName();
+    }
+}
