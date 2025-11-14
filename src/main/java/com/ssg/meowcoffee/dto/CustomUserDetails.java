09ab2a5faf6af1b969aeda2883cb917fd0a6455d
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-11-14 10:36:08 +0900

    fix(auth): 컨트롤러 요청 처리 시 사용자 권한 확인 오류 해결

--- src/main/java/com/ssg/meowcoffee/dto/CustomUserDetails.java
+++ src/main/java/com/ssg/meowcoffee/dto/CustomUserDetails.java
@@ -1,8 +1,10 @@
 package com.ssg.meowcoffee.dto;
 
+import com.ssg.meowcoffee.domain.UserRole;
 import com.ssg.meowcoffee.domain.UserStatus;
 import com.ssg.meowcoffee.domain.UserVO;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.GrantedAuthority;
@@ -14,30 +16,42 @@ import java.util.Collection;
 import java.util.List;
 
 @Log4j2
-@Data
+@Getter
 @RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
 
-    private final UserVO userVO;
+    private static final long serialVersionUID = 1L;
+
+    private String userId;
+    private String userPwd;
+    private UserRole userRole;
+    private UserStatus userStatus;
+
+    public CustomUserDetails(UserVO userVO) {
+        this.userId = userVO.getUserId();
+        this.userPwd = userVO.getUserPwd();
+        this.userRole = userVO.getUserRole();
+        this.userStatus = userVO.getUserStatus();
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         // 현재 로그인한 사용자가 보유한 권한을 반환
         List<GrantedAuthority> collection = new ArrayList<>();
-        collection.add(new SimpleGrantedAuthority("ROLE_" + userVO.getUserRole()));
+        collection.add(new SimpleGrantedAuthority("ROLE_" + this.userRole));
         return collection;
     }
 
     @Override
     public String getPassword() {
         // 현재 로그인한 사용자의 비밀번호를 반환
-        return userVO.getUserPwd();
+        return this.userPwd;
     }
 
     @Override
     public String getUsername() {
         // 현재 로그인한 사용자의 아이디를 반환
-        return userVO.getUserId();
+        return this.userId;
     }
 
     // 계정 만료, 잠금, 자격 증명 만료 관련 조건을 설정하는 부분
@@ -59,6 +73,6 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         // 로그인 가능한 조건을 지정(승인 완료된 계정이어야만 로그인 가능)
-        return userVO.getUserStatus() == UserStatus.APPROVAL;
+        return this.userStatus == UserStatus.APPROVAL;
     }
 }
